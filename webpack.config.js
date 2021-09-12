01ea3968bd04c009363c42a5a6437dd0e8342e6e
Norman Breau <norman@nbsolutions.ca>
2021-09-11 23:22:12 -0300

    disabled minimization (breaks css links

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,63 @@
 
 const Path = require('path');
 
-module.exports = {
-    entry: './src/api.ts',
-    devtool: 'source-map',
-    output: {
-        path: Path.resolve(__dirname, './lib'),
-        filename: 'application.js',
-        library: {
-            type: 'commonjs2'
-        },
-        clean: true
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                        options: {
-                            injectType: 'lazyStyleTag'
-                        }
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ],
-                exclude: /node_modules/,
-                include: Path.resolve(__dirname, 'src')
+module.exports = (env, argv) => {
+    // console.log('ENV', env, argv);
+    // let optimization;
+
+    // if (env.mode === 'development') {
+        
+    // }
+
+    return {
+        entry: './src/api.ts',
+        devtool: 'source-map',
+        output: {
+            path: Path.resolve(__dirname, './lib'),
+            filename: 'application.js',
+            library: {
+                type: 'commonjs2'
             },
-            {
-                test: /\.ts|\.tsx$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    externals: {
-        'react': 'react',
-        '@nbsolutions/interfaces': '@nbsolutions/interfaces',
-        '@nbsolutions/object': '@nbsolutions/object'
-    }
+            clean: true
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: 'style-loader',
+                            options: {
+                                injectType: 'lazyStyleTag'
+                            }
+                        },
+                        {
+                            loader: 'css-loader'
+                        },
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ],
+                    exclude: /node_modules/,
+                    include: Path.resolve(__dirname, 'src')
+                },
+                {
+                    test: /\.ts|\.tsx$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        externals: {
+            'react': 'react',
+            '@nbsolutions/interfaces': '@nbsolutions/interfaces',
+            '@nbsolutions/object': '@nbsolutions/object'
+        },
+        optimization: {
+            minimize: false
+        }
+    };
 };
